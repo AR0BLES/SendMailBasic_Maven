c8e3bf05a42e3d868d229ef3cc95a851f4ca92c1
Alexis <aroblesc14@gmail.com>
2022-12-03 18:09:35 -0500

    Agrego string alexis pruebas

--- src/main/java/Clases/Asistencia.java
+++ src/main/java/Clases/Asistencia.java
@@ -20,7 +20,7 @@ String Totalasistencia;
 String HoraporAsistencia;
 String TotalHora;
 String Mes;
-
+String Alexis;
     public Asistencia(String codigo, String Nombre, String Apellido, String Asistencia, String Tardanza, String Falta, String Justificada, String Totalasistencia, String HoraporAsistencia, String TotalHora, String Mes) {
         this.codigo = codigo;
         this.Nombre = Nombre;
