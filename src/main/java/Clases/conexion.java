f028b4b82c33c144a0c790d0fc372da5202086e6
Jose Chuchon Romani <117874667+josechuchon@users.noreply.github.com>
2022-12-05 19:28:43 -0500

    Ejecucion

--- src/main/java/Clases/conexion.java
+++ src/main/java/Clases/conexion.java
@@ -1,38 +1,41 @@
 // @author Jose Chuchon
 //Importacion de todos los paquetes para el funcionamiento de la clase  
+//IMportacion de librerias para conectar sql con java
 package Clases;
 import java.sql.*;
 import java.sql.DriverManager;
 import javax.swing.JOptionPane;
 
-
+//creacion de la clase
 public class conexion 
 {
-
     public static Statement createStatement() {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+        throw new UnsupportedOperationException("Not supported yet."); 
+    // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
     }
+    //Declaracion de variables para conectar el sql
     Connection conectar=null;
     String usuario="usersql";
     String contrasenia="root";
     String bd="dbPlanillaColegio";
     String ip="localhost\\SQLEXPRESS";
     String puerto="1433";
-   // public Connection cn=null;
-   // public Statement st =null;
-    
     String cadena="jdbc:sqlserver://"+ip+":"+puerto+"/"+bd;
    
+    //Declaracion de una clase para establecer la conexion con la base de datos
     public Connection establecerConexion(){
+        
+        //Uso de try-catch para verificar la funcionalidad de la conecccion 
        try{
+           //establecer la conexion
         String cadena="jdbc:sqlserver://localhost:"+puerto+";"+"datebaseName="+bd+";TrustServerCertificate=True;";
-       
         conectar =DriverManager.getConnection(cadena,usuario,contrasenia);
-     //   st=cn.createStatementResultSettement(createStatementResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_READ_ONLY);
-        //JOptionPane.showMessageDialog(null,"Ok, Conexión Correcta"); 
+    
        }catch (Exception e){
+           //Imprimir un mensaje cuando no se establece conexion con la base de datos
           JOptionPane.showMessageDialog(null,"Error al conectar a la base datos, error:"+ e.toString()+cadena+usuario+contrasenia); 
        } 
+       //Retorna la comprobacion de la conexion de nuevo
         return conectar;
     }
 }
